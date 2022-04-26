ac2d1e6fbd6cc712ad90149537510befac9fa847
Rahul Singh <mr.rahulsingh@hotmail.com>
2022-04-26 20:03:08 +0200

    Controller and view created

---
+++ Apotea_Interview/Controllers/GetZipCodesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Apotea_Interview.Controllers
+{
+    public class GetZipCodesController : Controller
+    {
+
+        Apotea_Student_InterviewQ2Entities db = new Apotea_Student_InterviewQ2Entities();
+        // GET: GetZipCodes
+        public ActionResult Index()
+        {
+            return View(db.Orders.ToList());
+        }
+        [HttpPost]
+        public ActionResult Index(DateTime StartDtae, DateTime Enddate)
+        {
+            return View(db.Func_GetZipCodes(StartDtae, Enddate));
+        }
+    }
+}
